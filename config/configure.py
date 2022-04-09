353ddebbfbc8b86aa9771a23d0762049e78824cc
SuryaKant <thakur.suryakant.0406@gmail.com>
2022-04-10 00:18:13 +0530

    config file path correction

--- config/configure.py
+++ config/configure.py
@@ -3,10 +3,10 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read("configs/config.ini")
 
-config.add_section('Main')
+config.add_section('User')
 
 
-with open('config.ini', 'w') as file:
+with open('configs/config.ini', 'w') as file:
     config.write(file)
 
 
