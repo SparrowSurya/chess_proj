d7128cea73c32646772d689c69b8ae340a177995
SuryaKant <thakur.suryakant.0406@gmail.com>
2022-04-18 22:34:05 +0530

    task.txt

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from src import ctxManager
 root = tk.Tk()
 # root.state("zoomed")
 root.geometry("660x660+420+10")
-root.config(bg='green')
+root.config(bg='#7d4512')
 
 c = tk.Canvas(root, height=cfg.SQSIZE*8, width=cfg.SQSIZE*8, highlightthickness=0)
 c.pack(padx=10, pady=10)
@@ -19,10 +19,11 @@ m = ctxManager(z)
 
 for i in range(8):
     for j in range(8):
-        z.cell(i, j).newimg(tk.PhotoImage(file=im.IWP))
+        z.cell(i, j).newimg(tk.PhotoImage(file=im.IWQ))
         z.cell(i, j).showimg()
 
 root.bind('<Button-1>', m.single_left_click_only)
 root.bind('<Button-3>', m.single_right_click_only)
 
 root.mainloop()
+
