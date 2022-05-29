69bb47c2f310b078231d59e1faabc4e748299547
SuryaKant <thakur.suryakant.0406@gmail.com>
2022-05-29 10:39:18 +0530

    new task +1, main gui class

---
+++ gui/__init__.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+from src import Brain
+from gui.chessboard import ChessBoard
+import config as cfg
+
+
+class Game():
+
+    def __init__(self):
+        self.__root: Tk = Tk()
+        self.__root.title("Chess")
+        self.__root.geometry("+640+20")
+        # self.self.__root.state("zoomed")
+
+        self.viewport: Canvas = Canvas(
+            self.__root,
+            bg='#7d4512',
+            height=cfg.SQSIZE*8+cfg.BOARD_BORDER,
+            width=cfg.SQSIZE*8+cfg.BOARD_BORDER,
+            highlightthickness=0
+        )
+        self.viewport.pack()
+
+        self.board: ChessBoard = ChessBoard(self.viewport)
+        self.game: Brain = Brain(self.board)
+        self.game.StartDefault()
+
+        self.__root.bind('<Button-1>', self.game.Mouse_SLC)
+        self.__root.bind('<Button-3>', self.game.Mouse_SRC)
+        self.__root.bind('<B1-Motion>', self.game.MouseDrag)
+        self.__root.bind('<ButtonRelease-1>', self.game.Mouse_LCR)
+
+    def run(self):
+        self.__root.mainloop()
--- test.py
+++ test.py
@@ -1,30 +1,9 @@
-import tkinter as tk
+from gui import Game
 
-import config as cfg
-from gui.chessboard import ChessBoard
-from src import Brain
+def main():
+    game = Game()
+    game.run()
 
 
-root = tk.Tk()
-root.geometry("+640+20")
-# root.state("zoomed")
-
-canvas = tk.Canvas(
-    root,
-    bg='#7d4512',
-    height=cfg.SQSIZE*8+cfg.BOARD_BORDER,
-    width=cfg.SQSIZE*8+cfg.BOARD_BORDER,
-    highlightthickness=0
-)
-canvas.pack()
-
-board = ChessBoard(canvas)
-b = Brain(board)
-b.StartDefault()
-
-root.bind('<Button-1>', b.Mouse_SLC)
-root.bind('<Button-3>', b.Mouse_SRC)
-root.bind('<B1-Motion>', b.MouseDrag)
-root.bind('<ButtonRelease-1>', b.Mouse_LCR)
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
